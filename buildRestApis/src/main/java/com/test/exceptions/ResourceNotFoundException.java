bb15bedbd5c406602d3b52b4db8520e57df4b935
Bhawna Maurya <91850085+bhawanamauryacodes@users.noreply.github.com>
2023-12-09 19:23:41 +0530

    Delete buildRestApis directory

--- buildRestApis/src/main/java/com/test/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.test.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-  
-	public ResourceNotFoundException(Long id) {
-		super("user not present with id=" +  id);
-	}
-}
--- buildRestApis/src/main/java/com/test/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.test.entity.User;
-
-public interface UserRepo extends JpaRepository<User,Long> {
-   
-}
